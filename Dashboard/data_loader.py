acf1e16faa2b5cd796c4c3dbcc598ce1bfc977b5
HUYNH TRI NHAN <huynhtrinhan6282710@gmail.com>
2024-12-05 21:39:02 +0700

    Cập nhật Chatbot

--- Dashboard/data_loader.py
+++ Dashboard/data_loader.py
@@ -1,23 +1,66 @@
 # data_loader.py
 import pandas as pd
-import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from sqlalchemy import create_engine
 import streamlit as st
+import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
+# PostgreSQL connection string
 conn_str = os.getenv('DATABASE_RENDER')
-engine = create_engine(conn_str)
 
+# Default columns for stock_data
+stock_data_necessary_columns = [
+    'trade_date',
+    'stock_code',
+    'listed_shares',
+    'shares_outstanding',
+    'reference_price',
+    'ceiling_price',
+    'floor_price',
+    'opening_price',
+    'closing_price',
+    'highest_price',
+    'lowest_price',
+    'price_change',
+    'price_change_percentage',
+    'market_capitalization',
+    'matched_orders_volume',
+    'matched_orders_value',
+    'agreements_volume',
+    'agreements_value',
+    'total_trading_volume',
+    'total_trading_value'
+]
+stock_index_necessary_columns = [
+    'trading_date',
+    'stock_code',
+    'closing_price',
+    'price_change',
+    'price_change_percentage',
+]
 
+columns_map = {
+    "stock_data": stock_data_necessary_columns,
+    "stock_index": stock_index_necessary_columns,
+}
 @st.cache_data
-def load_all_tables():
+def load_all_tables(columns_map=columns_map):
+    """
+    Load all tables from the database. Optionally, specify columns for each table using columns_map.
+    :param columns_map: A dictionary where the key is the table name and the value is a list of columns to query.
+    :return: A dictionary of DataFrames, keyed by table name.
+    """
     tables = ["stock_data", "stock_info", "financial_metrics", "stock_index"]
     data_frames = {}
 
     with ThreadPoolExecutor() as executor:
-        future_to_table = {executor.submit(load_data, table): table for table in tables}
+        future_to_table = {
+            executor.submit(load_data, table, columns_map.get(table,None) if columns_map else None): table 
+            for table in tables
+        }
         for future in as_completed(future_to_table):
             table = future_to_table[future]
             try:
@@ -26,15 +69,38 @@ def load_all_tables():
                 st.error(f"Error loading {table}: {e}")
                 data_frames[table] = pd.DataFrame()
 
-    print("Data loaded successfully !!!")
     return data_frames
 
+@st.cache_data
+def load_data(table_name, columns=None):
+  
+    engine = create_engine(conn_str)
+    # Build SQL query
+    columns_query = ", ".join(columns) if columns else "*"
+    query = f"SELECT {columns_query} FROM {table_name}"
+    
+    return pd.read_sql(query, engine)
 
 @st.cache_data
-def load_data(table_name):
-    return pd.read_sql(f"SELECT * FROM {table_name}", engine)
+def load_stock_data(columns=None):
 
+    return load_data("stock_data", columns if columns else stock_data_necessary_columns)
 
 @st.cache_data
-def load_stock_data():
-    return pd.read_sql("SELECT * FROM stock_data", engine)
+def load_article():
+    try:
+        load_dotenv()
+        conn_str = os.getenv('DATABASE_RENDER')
+
+        engine = create_engine(conn_str)
+
+        # Fetch data from the table
+        query = f"SELECT * FROM article;"
+        with engine.connect() as connection:
+            df = pd.read_sql(query, connection)
+        df['date'] = pd.to_datetime(df['date'])
+        
+        return df
+    except Exception as e:
+        print(e)
+       
